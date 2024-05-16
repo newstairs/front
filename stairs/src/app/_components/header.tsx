dee5bf05b59fa796881b2d67c655bc90dd7a5243
pulv1593 <jiro0727@naver.com>
2024-05-16 15:37:38 +0900

    fix: 헤더 컴포넌트 일부 수정#5

--- stairs/src/app/_components/header.tsx
+++ stairs/src/app/_components/header.tsx
@@ -3,21 +3,28 @@ import React from "react";
 
 const Header: React.FC = () => {
   
-  const items: string[] = ["장바구니", "메인 품목 리스트", "마트 별 선택 항목"];
+  const items: string[] = [ "메인 품목 리스트", "마트 별 선택 항목", "장바구니" ];
 
   const handleClick = (index: number, item: string): void => {
     console.log(`Clicked item: ${item}, Index: ${index}`);
   }
 
   return (
-    <header className="fixed top-0 w-full bg-black shadow-md">
-      <ul className="flex justify-end space-x-4 p-4">
-        {items.map((item, index) => (
-          <li key={index} onClick={() => handleClick(index, item)}>
-            {item}
-          </li>
-        ))}
-      </ul>
+    <header className="fixed top-0 w-full shadow-md">
+      <div className="flex justify-between">
+        <div className="flex items-center pl-6">
+          <button className=""> 로그아웃 </button>
+        </div>
+        <div>
+          <ul className="flex space-x-4 p-4">
+            {items.map((item, index) => (
+              <li key={index} onClick={() => handleClick(index, item)}>
+                {item}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </header>
   );
 }
