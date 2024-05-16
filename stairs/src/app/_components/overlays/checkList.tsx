6bf793d4266a2b57c842be66447948e049f71bbc
조우석 <dntjrwh@gmail.com>
2024-05-16 16:54:24 +0900

    선택항목리스트 css 적용 #6

---
+++ stairs/src/app/_components/overlays/checkList.tsx
@@ -0,0 +1,14 @@
+'use client';
+import React from "react";
+import '../../styles/overlayContainer.css';
+
+const CheckList: React.FC = () => {
+  
+  return (
+    <div className="overlay-container">
+
+    </div>
+);
+}
+
+export default CheckList;
