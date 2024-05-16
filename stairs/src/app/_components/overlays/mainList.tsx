a14c7b9b276470ed80ecd80a91355357477cb19e
조우석 <dntjrwh@gmail.com>
2024-05-16 16:54:11 +0900

    메인 품목 리스트 css 적용 #3

---
+++ stairs/src/app/_components/overlays/mainList.tsx
@@ -0,0 +1,14 @@
+'use client';
+import React from "react";
+import '../../styles/overlayContainer.css';
+
+const MainList: React.FC = () => {
+  
+  return (
+    <div className="overlay-container">
+
+    </div>
+);
+}
+
+export default MainList;
