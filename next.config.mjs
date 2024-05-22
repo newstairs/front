9443f41dda443a9662e2b6ee79a99ed535ed7532
pulv1593 <jiro0727@naver.com>
2024-05-22 19:39:47 +0900

    fix: 환경변수 관련 코드 수정#7

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,3 @@
-const fs = require('fs');
-const path = require('path');
-const yaml = require('js-yaml');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -13,37 +10,15 @@ const nextConfig = {
       },
     ];
   },
+  env: {
+    NEXT_PUBLIC_REST_API_KEY: process.env.NEXT_PUBLIC_REST_API_KEY,
+    NEXT_PUBLIC_JAVASCRIPT_API_KEY: process.env.NEXT_PUBLIC_JAVASCRIPT_API_KEY,
+    NEXT_PUBLIC_FRONT_REDIRECT_URL: process.env.NEXT_PUBLIC_FRONT_REDIRECT_URL,
+  },
 };
+// 디버깅: 로드된 환경 변수를 출력합니다
+console.log('Loaded ENV:', process.env.NEXT_PUBLIC_REST_API_KEY);
+console.log('Loaded ENV:', process.env.NEXT_PUBLIC_JAVASCRIPT_API_KEY);
+console.log('Loaded ENV:', process.env.NEXT_PUBLIC_FRONT_REDIRECT_URL);
 
-const decodeBase64 = (encoded) => {
-  return Buffer.from(encoded, 'base64').toString('utf8');
-};
-
-// YAML 파일에서 환경 변수를 로드
-const loadEnvFromYaml = () => {
-  try {
-    const envPath = path.resolve(__dirname, 'secrets.yaml');
-    const fileContents = fs.readFileSync(envPath, 'utf8');
-    const data = yaml.load(fileContents);
-
-    for (const key in data) {
-      if (Object.prototype.hasOwnProperty.call(data, key)) {
-        process.env[key] = decodeBase64(data[key]);
-      }
-    }
-  } catch (e) {
-    console.error('Error loading environment variables from YAML:', e);
-  }
-};
-
-// 환경 변수를 로드합니다
-loadEnvFromYaml();
-
-// Next.js 환경 변수 설정
-nextConfig.env = {
-  NEXT_PUBLIC_REST_API_KEY: process.env.NEXT_PUBLIC_REST_API_KEY,
-  NEXT_PUBLIC_JAVASCRIPT_API_KEY: process.env.NEXT_PUBLIC_JAVASCRIPT_API_KEY,
-  NEXT_PUBLIC_FRONT_REDIRECT_URL: process.env.NEXT_PUBLIC_FRONT_REDIRECT
-};
-
-module.exports = nextConfig;
+export default nextConfig;
--- src/app/_components/map/Map.tsx
+++ src/app/_components/map/Map.tsx
@@ -12,6 +12,8 @@ interface MapProps {
 
 const Map: React.FC<MapProps> = ({ location }) => {
   const kakao_map_api_key = process.env.NEXT_PUBLIC_JAVASCRIPT_API_KEY;
+  console.log('Kakao Map API Key:', kakao_map_api_key);
+  console.log('NEXT_PUBLIC_JAVASCRIPT_API_KEY:', process.env.NEXT_PUBLIC_JAVASCRIPT_API_KEY);
 
   // @ts-ignore
   const mapRef = useRef<kakao.maps.Map | null>(null);
