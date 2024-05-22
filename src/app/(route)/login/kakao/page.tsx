13cfda9e376d95aed8a31b5b25845a22bfa2f052
pulv1593 <jiro0727@naver.com>
2024-05-22 17:41:22 +0900

    fix: 로그인 페이지 코드 수정#4

--- src/app/(route)/login/kakao/page.tsx
+++ src/app/(route)/login/kakao/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Page: React.FC = () => {
   return (
     <div className="h-screen flex justify-center items-center">
-      <LoginHandler />
+      {/* <LoginHandler /> */}
     </div>
   )
 }
