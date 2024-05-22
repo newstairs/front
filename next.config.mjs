564073c3db3c45f832fb64da76db0833475af9a2
pulv1593 <jiro0727@naver.com>
2024-05-22 18:39:24 +0900

    chore: 환경변수 관련 코드 수정#7

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,45 @@
+const fs = require('fs');
+const path = require('path');
+const yaml = require('js-yaml');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  
   async redirects() {
     return [
       {
-        source: "/",
-        destination: "/login",
+        source: '/',
+        destination: '/login',
         permanent: true,
       },
     ];
   },
 };
 
-export default nextConfig;
+// YAML 파일에서 환경 변수를 로드
+const loadEnvFromYaml = () => {
+  try {
+    const envPath = path.resolve(__dirname, 'secrets.yaml');
+    const fileContents = fs.readFileSync(envPath, 'utf8');
+    const data = yaml.load(fileContents);
+
+    for (const key in data) {
+      if (Object.prototype.hasOwnProperty.call(data, key)) {
+        process.env[key] = data[key];
+      }
+    }
+  } catch (e) {
+    console.error('Error loading environment variables from YAML:', e);
+  }
+};
+
+// 환경 변수를 로드합니다
+loadEnvFromYaml();
+
+// Next.js 환경 변수 설정
+nextConfig.env = {
+  NEXT_PUBLIC_REST_API_KEY: process.env.NEXT_PUBLIC_REST_API_KEY,
+  NEXT_PUBLIC_JAVASCRIPT_API_KEY: process.env.NEXT_PUBLIC_JAVASCRIPT_API_KEY,
+  NEXT_PUBLIC_FRONT_REDIRECT_URL: process.env.NEXT_PUBLIC_FRONT_REDIRECT
+};
+
+module.exports = nextConfig;
