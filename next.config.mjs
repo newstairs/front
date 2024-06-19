15538653a903c08be9717ee8015249b277b533e3
pulv1593 <jiro0727@naver.com>
2024-06-20 01:45:44 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -9,22 +9,22 @@ const nextConfig = {
       },
     ];
   },  
-  async rewrites() {
-    return [
-      {
-        source: "/reviews/detail/:id",
-        destination: "/reviews/detail/:id",
-      },
-      {
-        source:"/friend/:name/:id/:martname",
-        destination:"/friend/:name/:id/:martname"
-      },
-      {
-        source: "/:path*",
-        destination: "http://ec2-15-164-104-176.ap-northeast-2.compute.amazonaws.com:8080/:path*",
-      }
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/reviews/detail/:id",
+  //       destination: "/reviews/detail/:id",
+  //     },
+  //     {
+  //       source:"/friend/:name/:id/:martname",
+  //       destination:"/friend/:name/:id/:martname"
+  //     },
+  //     {
+  //       source: "/:path*",
+  //       destination: "http://ec2-15-164-104-176.ap-northeast-2.compute.amazonaws.com:8080/:path*",
+  //     }
+  //   ];
+  // },
   reactStrictMode:false
 };
 
